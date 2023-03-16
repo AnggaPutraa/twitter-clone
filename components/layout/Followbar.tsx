375ab15841131fa3f214dae2148ac535190021b0
AnggaPutraa <yananggaa@gmail.com>
2023-03-17 01:58:04 +0700

    refactor: reuseable header

--- components/layout/Followbar.tsx
+++ components/layout/Followbar.tsx
@@ -5,7 +5,7 @@ const Followbar = () => {
             <div className="border border-neutral-800 rounded-xl p-4">
                 <h2 className="text-white text-lg font-semibold">Who to follow</h2>
                 <div className="flex flex-col gap-6 ml-4">
-
+                    
                 </div>
             </div>
         </div>
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,9 @@
+import Header from "@/components/Header";
 
 export default function Home() {
   return (
-    <div className="text-white">
-      
-    </div>
+    <>
+      <Header showBackArrow label='Home' />
+    </>
   )
 }
