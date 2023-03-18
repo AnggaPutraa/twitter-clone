ca00912a0bf8b58d71ff1f0c24f0917413dd1a99
AnggaPutraa <yananggaa@gmail.com>
2023-03-18 21:29:06 +0700

    feat: implement upload image

--- components/modals/EditModal.tsx
+++ components/modals/EditModal.tsx
@@ -6,6 +6,7 @@ import { toast } from 'react-hot-toast';
 import axios from 'axios';
 import Modal from "../Modal";
 import Input from "../Input";
+import ImageUpload from "../users/ImageUpload";
 
 const EditModal = () => {
     const { data: currentUser } = useCurrentUser();
@@ -56,6 +57,18 @@ const EditModal = () => {
 
     const bodyContent = (
         <div className="flex flex-col gap-4">
+            <ImageUpload
+                value={profileImage}
+                disabled={isLoading}
+                onChange={(image) => setProfileImage(image)}
+                label='Upload profile Image'
+            />
+            <ImageUpload
+                value={coverImage}
+                disabled={isLoading}
+                onChange={(image) => setCoverImage(image)}
+                label='Upload cover Image'
+            />
             <Input
                 placeHolder="Name"
                 onChange={(e) => setName(e.target.value)}
