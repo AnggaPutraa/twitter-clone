85abb099f26726e7430cc7a6ae975d30251e2faf
AnggaPutraa <yananggaa@gmail.com>
2023-03-20 20:03:42 +0700

    fix: current user not added  to following user

--- components/layout/Followbar.tsx
+++ components/layout/Followbar.tsx
@@ -1,8 +1,10 @@
+import useCurrentUser from "@/hooks/useCurrentUser";
 import useUsers from "@/hooks/useUsers";
 import Avatar from "../Avatar";
 
 const Followbar = () => {
     const { data: users = [] } = useUsers();
+    const { data: currentUser } = useCurrentUser();
 
     if (users.lenght == 0) {
         return null;
@@ -15,19 +17,21 @@ const Followbar = () => {
                 <div className="flex flex-col gap-6 mt-4">
                     {users.map(
                         (user: Record<string, any>) => (
-                            <div key={user.id} className="flex flex-row gap-4">
-                                <Avatar
-                                    userId={user.id}
-                                />
-                                <div className="flex flex-col">
-                                    <p className="text-white font-semibold text-sm">
-                                        {user.name}
-                                    </p>
-                                    <p className="text-neutral-400 text-sm">
-                                        @{user.username}
-                                    </p>
+                            user.id != currentUser?.id ? (
+                                <div key={user.id} className="flex flex-row gap-4">
+                                    <Avatar
+                                        userId={user.id}
+                                    />
+                                    <div className="flex flex-col">
+                                        <p className="text-white font-semibold text-sm">
+                                            {user.name}
+                                        </p>
+                                        <p className="text-neutral-400 text-sm">
+                                            @{user.username}
+                                        </p>
+                                    </div>
                                 </div>
-                            </div>
+                            ) : null
                         )
                     )}
                 </div>
